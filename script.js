106d6e7bb8db242e6f0b8a4b251d8b49c8bae349
Musraf <mussraffro45@gmail.com>
2025-03-08 09:55:16 +0530

    changes

--- script.js
+++ script.js
@@ -1,105 +1,87 @@
-const balance=document.querySelector("#balance");
-const inc_amt =document.querySelector("#inc-amt");
-const exp_amt=document.querySelector("#exp-amt");
-const trans=document.querySelector("#trans");
-const form=document.querySelector("#form");
-const description=document.querySelector("#desc");
-const amount=document.querySelector("#amount");
-/*
-const dummyData =[
-    { id:1, description: "Rent", amount: -20},
-    { id:2, description: "Salary", amount: 35000},
-    { id:3, description: "Book", amount: -10},
-    { id:4, description: "Fees", amount: -150},
-    { id:5, description: "Petrol", amount: -250},
-];
+let expenses =[];
+let totalAmount = 0;
 
-let transactions = dummyData;
-*/
-const localStorageTrans = JSON.parse(localStorage.getItem("trans"));
-let transactions = localStorage.getItem("trans") !== null ?
-localStorageTrans : [];
+const categorySelect = document.getElementById('category-select')
+const amountInput = document.getElementById('amount-input')
+const dateInput = document.getElementById('date-input')
+const addBtn = document.getElementById('add-btn')
+const expenseTableBody = document.getElementById('expense-table-body')
+const totalAmountCell = document.getElementById('total-amount')
 
-function loadTransactionDetails(transaction){
-    const sign= transaction.amount < 0 ?"-" : "+";
-    const item=document.createElement("li")
-    item.classList.add(transaction.amount < 0 ? "exp1" : "inc1");
-    trans.appendChild(item);
-    item.innerHTML=`
-    ${transaction.description}
-    <span>${sign} ${Math.abs(transaction.amount)}</span>
-    <button class="btn-del" onclick="removeTrans(${transaction.id})">X</button>
-    `;
-    //console.log(transaction);
-}
+addBtn.addEventListener('click', function(){
+    const category = categorySelect.value;
+    const amount = Number(amountInput.value);
+    const date= dateInput.value;
 
-function removeTrans(id){
-    if(confirm("Are you sure you want to delete Transaction?")){
-       transactions =transactions.filter((transaction) => transaction.id != id);
-       config();
-       updateLocalStorage();
-    }else{
+    if(category === ''){
+        alert('please select a category');
         return;
     }
-}
-function updateAmount(){
-     const amounts=transactions.map((transaction) =>
-    transaction.amount);
-     const total = amounts.reduce((acc, item) => (acc += item),
-    0).toFixed(2);
-    balance.innerHTML =`₹ ${total}`;
-
-         const income = amounts
-         .filter((item) => item > 0)
-         .reduce((acc, item) => (acc += item), 0)
-         .toFixed(2);
-        inc_amt.innerHTML =`₹ ${income}`;
-
-        const expense = amounts
-         .filter((item) => item < 0)
-         .reduce((acc, item) => (acc += item), 0)
-         .toFixed(2);
-        exp_amt.innerHTML =`₹ ${expense}`;
-    
-    
-}
-
-function config(){
-    trans.innerHTML = ""; 
-    transactions.forEach(loadTransactionDetails);
-    updateAmount();
-}
-    function  addTransaction(e){
-        e.preventDefault();
-        if (description.value.trim() == "" || amount.value.trim()
-            == ""){
-               alert("please Enter Description and amount")
-            }else{
-                const transaction={
-                    id:uniqueId(),
-                    description: description.value,
-                    amount: +amount.value,
-                };
-                transactions.push(transaction);
-                loadTransactionDetails(transaction);
-                description.value= "";
-                amount.value = "";
-                updateAmount();
-                updateLocalStorage();
-            }
-        
+    if(isNaN(amount) || amount <= 0){
+        alert('please enter a valid amount');
+        return;
     }
-
-    function uniqueId(){
-        return Math.floor(Math.random() * 1000);
+    if(date ===''){
+        alert('please select a date');
+        return;
     }
+    expenses.push({category,amount,date})
 
-form.addEventListener("submit" , addTransaction);
+    totalAmount += amount;
+    totalAmountCell.textContent=totalAmount;
 
-window.addEventListener("load", function () {
-    config();
+    const newRow = expenseTableBody.insertRow();
+
+    const categoryCell = newRow.insertCell();
+    const AmountCell = newRow.insertCell();
+    const dateCell = newRow.insertCell();
+    const deleteCell = newRow.insertCell();
+
+    const deleteBtn = document.createElement('button');
+
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.classList.add('delete-btn');
+    deleteBtn.addEventListener('click', function(){
+        expenses.splice(expenses.indexOf(expense),1);
+
+        totalAmount -= expense.amount;
+        totalAmountCell.textContent = totalAmount;
+
+        expenseTableBody.removeChild(newRow);
+    })
+    const expense= expenses[expenses.length - 1];
+    categoryCell.textContent=expense.category;
+    AmountCell.textContent=expense.amount;
+    dateCell.textContent=expense.date;
+    deleteCell.appendChild(deleteBtn);
+
 });
+for(const exepense of expenses){
+    totalAmount += amount;
+    totalAmountCell.textContent=totalAmount;
 
-function updateLocalStorage(){
-    localStorage.setItem("trans", JSON.stringify(transactions));
+    const newRow = expenseTableBody.insertRow();
+
+    const categoryCell = newRow.insertCell();
+    const AmountCell = newRow.insertCell();
+    const dateCell = newRow.insertCell();
+    const deleteCell = newRow.insertCell();
+
+    const deleteBtn = document.createElement('button');
+
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.classList.add('delete-btn');
+    deleteBtn.addEventListener('click', function(){
+        expenses.splioce(expenses.indexOf(expense),1);
+
+        totalAmount -= expense.amount;
+        totalAmountCell.textContent = totalAmount;
+
+        expenseTableBody.removeChild(newRow);
+    })
+    const expense= expenses[expenses.length - 1];
+    categoryCell.textContent=expense.category;
+    AmountCell.textContent=expense.amount;
+    dateCell.textContent=expense.date;
+    deleteCell.appendChild(deleteBtn);
 }
